5e8786dc8dc62e8beca1bded5282fbe79b7b6bc1
upanan82 <upanan82@gmail.com>
2023-05-09 01:29:49 +0300

    Updated config class

--- src/config.py
+++ src/config.py
@@ -1,41 +1,18 @@
-from argparse import ArgumentParser, BooleanOptionalAction
-from configparser import ConfigParser
-from enum import Enum
+import json
 from typing import Any
 
+from utils.property import deep_value
 
-class ConfigType(Enum):
-  STR = 1
-  INT = 2
-  BOOL = 3
 
 class Config:
-  _parser: ConfigParser = ConfigParser()
-  _is_prod: bool = False
+  _data: dict[str, Any] = {}
   
-  def load():
-    # initialize command-line argument parser
-    ap: ArgumentParser = ArgumentParser()
-    # declare an argument for the prod environment switch
-    ap.add_argument('--prod', default = False, action = BooleanOptionalAction)
-    # extract command-line arguments
-    args: dict[str, Any] = vars(ap.parse_args())
-    
-    Config._is_prod = args['prod']
-    
+  @staticmethod
+  def load(file: str):
     # load configuration file
-    Config._parser.read('config{}.ini'.format('.prod' if Config._is_prod else ''))
-  
-  @property
-  @staticmethod
-  def is_prod() -> int:
-    return Config._is_prod
+    with open(file, 'r') as fp:
+      Config._data = dict(json.load(fp))
   
   @staticmethod
-  def get(section: str, option: str, type = ConfigType.STR) -> (int | bool | str):
-    if type == ConfigType.INT:
-      return Config._parser.getint(section, option)
-    elif type == ConfigType.BOOL:
-      return Config._parser.getboolean(section, option)
-    
-    return Config._parser.get(section, option)
+  def get(path: str) -> Any:
+    return deep_value(Config._data, path)
