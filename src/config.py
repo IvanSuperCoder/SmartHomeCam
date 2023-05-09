6b6d857c4186f0b9eb273bd728c8437817ee1478
upanan82 <upanan82@gmail.com>
2023-05-10 02:25:02 +0300

    Updated config

--- src/config.py
+++ src/config.py
@@ -1,22 +1,27 @@
-import os
 from typing import Any
 
 import yaml
 
-from utils.property import deep_value
+from src.utils.property import deep_value
 
 
 class Config:
   _data: dict[str, Any] = {}
-  
-  is_prod: bool = os.environ['ENV'] == 'prod'
+  _is_prod: bool = False
   
   @staticmethod
-  def init():
+  def init(is_prod: bool = False):
+    Config._is_prod = is_prod
+    
     # load configuration file
-    with open(f"config.{os.environ['ENV']}.yaml", 'r') as fp:
+    with open(f"config{'.prod' if is_prod else ''}.yaml", 'r') as fp:
       Config._data = yaml.load(fp, yaml.Loader)
   
   @classmethod
   def get(cls, path: str) -> Any:
     return deep_value(cls._data, path)
+  
+  @classmethod
+  @property
+  def is_prod(cls) -> bool:
+    return cls._is_prod
